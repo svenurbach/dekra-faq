fd191e114448b35abdfbe179c23ccec9bc78ff37
Sven Urbach <urbachsven@gmail.com>
2025-07-18 20:12:20 +0200

    add first page

---
+++ resources/js/Pages/Faq.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello welcome to your first Inertia app!</p>
+</template>
