5fe88e8992eb70e2cf771424481f584f84885fc7
Sven Urbach <urbachsven@gmail.com>
2025-07-25 16:38:04 +0200

    add background elements

--- resources/js/Components/Admin/AdminHeader.vue
+++ resources/js/Components/Admin/AdminHeader.vue
@@ -1,10 +1,22 @@
 <script setup>
+import Triangle from '@assets/design-elements/triangle-bright-green.svg';
 </script>
 
 <template>
   <main>
-    <header class="flex flex-col items-center py-[calc(var(--app-padding)*4)] pt-[calc(var(--app-padding)*11)] bg-(--clr-darkgreen-800) text-(--clr-white)">
-      <h1><slot name="headline" /></h1>
+    <header
+      class="relative flex flex-col items-center py-[calc(var(--app-padding)*4)] pt-[calc(var(--app-padding)*11)] bg-(--clr-darkgreen-800) text-(--clr-white)">
+      <div class="absolute inset-0 overflow-hidden">
+        <Triangle class="absolute w-34 -left-5 top-1/7 rotate-0 fill-(--clr-brightgreen-300)" />
+        <Triangle class="absolute w-20 left-14 top-3/7 rotate-180 fill-(--clr-darkgreen-400)" />
+        <Triangle class="absolute w-40 left-50 -bottom-15 rotate-0 fill-(--clr-brightgreen-400)" />
+        <Triangle class="absolute w-50 right-20 -top-1 rotate-180 fill-(--clr-darkgreen-300)" />
+        <Triangle class="absolute w-30 right-28 top-48 rotate-0 fill-(--clr-brightgreen-300)" />
+        <Triangle class="absolute w-40 right-70 bottom-0 rotate-180 fill-(--clr-darkgreen-400)" />
+      </div>
+      <h1>
+        <slot name="headline" />
+      </h1>
       <p class="w-[80ch] text-center">
         <slot name="subline" />
       </p>
