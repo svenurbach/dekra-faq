9474da1b9d66aa07044b793697ef8fa46043fde1
Sven Urbach <urbachsven@gmail.com>
2025-07-25 15:12:48 +0200

    minor header and footer

--- resources/js/Components/AppFooter.vue
+++ resources/js/Components/AppFooter.vue
@@ -1,9 +1,5 @@
-<script setup>
-
-</script>
-
 <template>
-  <footer class="text-center mb-(--app-padding)">
+  <footer class="text-center mb-(--app-padding) mt-[calc(var(--app-padding)*6)]">
     <div>2025 | DEKRA Neo GmbH</div>
   </footer>
 </template>
--- resources/js/Components/AppHeader.vue
+++ resources/js/Components/AppHeader.vue
@@ -1,14 +1,12 @@
 <script setup>
-import AppNavbar from './AppNavbar.vue';
 import AppSearch from './AppSearch.vue';
 </script>
 
 <template>
   <main>
-    <AppNavbar />
     <header class="flex flex-col items-center p-[calc(var(--app-padding)*4)] py-[calc(var(--app-padding)*6)] pt-[calc(var(--app-padding)*11)] bg-(--clr-darkgreen-800) text-(--clr-white)">
       <h1>Häufig gestellte Fragen</h1>
-      <p class="w-[80ch]">Willkommen in unserem Hilfebereich! Hier finden Sie eine Sammlung der häufigsten Fragen und Antworten zu unseren Angeboten und Services. Finden Sie schnell und unkompliziert die Informationen, die Sie suchen.</p>
+      <p class="w-[80ch] text-center">Willkommen in unserem Hilfebereich! Hier finden Sie eine Sammlung der häufigsten Fragen und Antworten zu unseren Angeboten und Services. Finden Sie schnell und unkompliziert die Informationen, die Sie suchen.</p>
       <div class="pt-[calc(var(--app-padding)*2)]">
         <AppSearch />
       </div>
