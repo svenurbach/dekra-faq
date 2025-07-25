5d7e00dadb293f7e8c61961fca2d88db932635ff
Sven Urbach <urbachsven@gmail.com>
2025-07-25 15:10:23 +0200

    change from tag to categories

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -4,25 +4,38 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use App\Models\Faq;
-use App\Models\Tag;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
-  public function index(Request $request)
-  {
-      $tag = $request->query('tag');
+    public function index(Request $request)
+    {
+        $category = $request->query('category');
+        $search = $request->query('search');
 
-      $faqs = Faq::with('tags')
-          ->when($tag, function ($query, $tag) {
-              $query->whereHas('tags', fn ($q) => $q->where('name', $tag));
-          })
-          ->get();
+        $faqs = Faq::with('category')
+            ->when($search && !$category, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('question', 'like', "%{$search}%")
+                        ->orWhere('answer', 'like', "%{$search}%")
+                        ->orWhereHas(
+                            'category',
+                            fn($categoryQuery) =>
+                            $categoryQuery->where('name', 'like', "%{$search}%")
+                        );
+                });
+            })
+            ->when($category && !$search, function ($query) use ($category) {
+                $query->whereHas('category', fn($q) => $q->where('name', $category));
+            })
+            ->get();
 
-      return Inertia::render('Faq', [
-          'faqs' => $faqs,
-          'tags' => Tag::all(),
-          'activeTag' => $tag,
-      ]);
-  }
+        return Inertia::render('Faq', [
+            'faqs' => $faqs,
+            'categories' => Category::all(),
+            'activeCategory' => $category,
+            'search' => $search,
+        ]);
+    }
 }
