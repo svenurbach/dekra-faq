0a568775982e914e2e49105b60ebd1b97ff3f27a
Sven Urbach <urbachsven@gmail.com>
2025-07-25 15:11:04 +0200

    svg loader config

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,36 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-        vue()
-    ],
-    resolve: {
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+    vue(),
+    svgLoader({
+      svgo: true,
+      svgoConfig: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'removeAttrs',
+            params: {
+              attrs: ['fill', 'width', 'height', 'clip-path']
+            },
+          },
+          { name: 'removeViewBox', active: false },
+        ],
+      },
+    })
+  ],
+  resolve: {
     alias: {
       '@assets': fileURLToPath(new URL('./resources/assets', import.meta.url)),
+      '@icons': fileURLToPath(new URL('./resources/assets/icons', import.meta.url)),
     },
   },
 });
