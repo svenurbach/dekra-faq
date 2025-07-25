40d80d88a27619f8f4d2b66ebab06a02a0145ad8
Sven Urbach <urbachsven@gmail.com>
2025-07-25 15:43:19 +0200

    login styling

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3'
+import { Head } from '@inertiajs/vue3'
+import AdminHeader from '@/Components/Admin/AdminHeader.vue'
+import AppButton from '@/Components/AppButton.vue'
 
 const form = useForm({
   name: '',
@@ -12,23 +15,30 @@ function submit() {
 </script>
 
 <template>
+    <Head title="Dekra | Login" />
+  <AdminHeader>
+    <template #headline>
+      Anmeldung
+    </template>
+    <template #subline>
+        Hier können Sie sich anmelden, um auf die Verwaltungsfunktionen zuzugreifen.
+    </template>
+  </AdminHeader>
   <div class="max-w-md mx-auto mt-10">
-    <h1 class="text-xl font-bold mb-4">Login</h1>
-
-    <form @submit.prevent="submit">
-      <div class="mb-4">
-        <label class="block mb-1">Benutzername</label>
-        <input v-model="form.name" type="text" class="w-full border p-2" />
-        <div v-if="form.errors.name" class="text-red-600 text-sm">{{ form.errors.name }}</div>
+    <form @submit.prevent="submit" class="flex flex-col gap-8">
+      <div >
+        <div class="mb-4">
+          <label class="block mb-1">Benutzername</label>
+          <input v-model="form.name" type="text" class="w-full rounded-lg border p-2" />
+          <div v-if="form.errors.name" class="text-(--clr-red-500) text-sm">{{ form.errors.name }}</div>
+        </div>
+        <div>
+          <label class="block mb-1">Passwort</label>
+          <input v-model="form.password" type="password" class="w-full rounded-lg border p-2" />
+          <div v-if="form.errors.password" class="text-(--clr-red-500) text-sm">{{ form.errors.password }}</div>
+        </div>
       </div>
-
-      <div class="mb-4">
-        <label class="block mb-1">Passwort</label>
-        <input v-model="form.password" type="password" class="w-full border p-2" />
-        <div v-if="form.errors.password" class="text-red-600 text-sm">{{ form.errors.password }}</div>
-      </div>
-
-      <button type="submit" class="bg-blue-600 text-white px-4 py-2">Login</button>
+      <AppButton @click="submit" title="Anmelden" class="bg-(--clr-darkgreen-500) text-(--clr-brightgreen-200)" />
     </form>
   </div>
 </template>
