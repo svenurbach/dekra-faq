8cb6b5096436c2806ed4e27b32252ca3e5329c1d
Sven Urbach <urbachsven@gmail.com>
2025-07-26 13:44:58 +0200

    add sort order attr

--- database/migrations/2025_07_18_111851_create_faqs_table.php
+++ database/migrations/2025_07_18_111851_create_faqs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('question');
             $table->text('answer');
+            $table->integer('sort_order')->default(0);
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
